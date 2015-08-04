e684de4a6cc93c4c736b987fa601e9d3f6f6f7ff
Robby Hays <rhaysvm42@gmail.com>
2015-08-03 22:58:27 -0700

    Visualizer now responding to signal strength of different frequency ranges! Fine tuning to come

--- public/app.js
+++ public/app.js
@@ -16,13 +16,9 @@ var context = new AudioContext;
 var analyser = context.createAnalyser();
 analyser.fftSize = 256;
 
-// var bassAnalyser = context.createAnalyser();
-// bassAnalyser.fftSize = 2048;
-
 var stream = document.querySelector('#audio');
-var frequencyData = new Float32Array(analyser.frequencyBinCount);
-setInterval(function(){console.log(frequencyData)}, 5000);
-// var bassFrequencyData = new Float32Array(analyser.frequencyBinCount);
+
+// var freqDomain = new Float32Array(analyser.frequencyBinCount);
 
 // Play stream and start basic pulse visuals
 stream.addEventListener('canplay', function() {
@@ -78,25 +74,91 @@ for (var i = 0; i < bass; i++) {
 
   bassNodes.push(node);
 }
+// var barray = [];
+// var marray = [];
+// var tarray = [];
 
 var pulse = function() {
-  analyser.getFloatFrequencyData(frequencyData);    
-  env.selectAll('.trebNode')
-   .data(frequencyData)
-   .style('transform', function(d) {
-     return 'scale(' + Math.abs(d)/10 + ')';
-   });
-// };
-   env.selectAll('.midNode')
-   .data(frequencyData)
-   .style('transform', function(d) {
-     return 'scale(' + Math.abs(d)/20 + ')';
-   });
-   env.selectAll('.bassNode')
-   .data(frequencyData)
-   .style('transform', function(d) {
-     return 'scale(' + Math.abs(d)/40 + ')';
-   });
+
+  // monitoring frequency Domain
+  var freqDomain = new Float32Array(analyser.frequencyBinCount);
+  analyser.getFloatFrequencyData(freqDomain);
+
+  // getFreqVal(x-Hz) should return peak value when analyzing x-Hz sound wave
+  var getFreqVal = function (freq) {
+    var nyquist = context.sampleRate / 2;
+    var index = Math.round(freq/nyquist * freqDomain.length);
+    return freqDomain[index];
+  };
+
+  // set up helper functions to analyze bass/mid/treb average signal strength
+  // @ given moment in time
+    // when these averages exceed threshold for a range --> trigger animation
+  var bassAnalysis = function(){
+    var min = 0; //0 Hz
+    var max = 320; //320 Hz
+    var step = (max - min) / 20;
+
+    var sum = 0;
+    for (var i = min; i < max; i += step){
+      sum += getFreqVal(i);
+    }
+    // barray.push(sum / 20);
+    return sum / 20;
+  };
+
+  var midAnalysis = function(){
+    var min = 320; //320 Hz
+    var max = 1280; //1280 Hz
+    var step = (max - min) / 60;
+
+    var sum = 0;
+    for (var i = min; i < max; i += step){
+      sum += getFreqVal(i);
+    }
+    // marray.push(sum / 60);
+    return sum / 60;
+  };
+
+  var trebAnalysis = function(){
+    var min = 1280; //0 Hz
+    var max = 20840; //20840 Hz
+    var step = (max - min) / 1220;
+
+    var sum = 0;
+    for (var i = min; i < max; i += step){
+      sum += getFreqVal(i);
+    }
+    // tarray.push(sum / 1220);
+    return sum / 1220;
+  };
+
+  // console.log(
+  //   bassAnalysis(),
+  //   midAnalysis(),
+  //   trebAnalysis()
+  //   );
+  if (trebAnalysis() > -70.31){
+    env.selectAll('.trebNode')
+      .data(freqDomain)
+      .style('transform', function(d) {
+        return 'scale(' + Math.abs(d)/10 + ')';
+    });
+  }
+  if (midAnalysis() > -44.17){
+    env.selectAll('.midNode')
+    .data(freqDomain)
+    .style('transform', function(d) {
+      return 'scale(' + Math.abs(d)/20 + ')';
+    });
+  }
+  if (bassAnalysis() > -32.06){
+    env.selectAll('.bassNode')
+    .data(freqDomain)
+    .style('transform', function(d) {
+      return 'scale(' + Math.abs(d)/40 + ')';
+    });
+  }
 };
 
 })();
